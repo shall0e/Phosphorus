0d3f02a21de50b56185afb4151083b0040a40815
Shall0e <shallowsspicymemes@gmail.com>
2024-11-29 23:36:20 -0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -11,11 +11,11 @@ export async function onRequest(context) {
     let response = await (await fetch("https://rers.shall0e.workers.dev/?"+((context.request.url).split("?"))[1])).text()
 
     if (isJsonString(response)) {
-        return new Response(JSON.stringify(response), {
+        return new Response(JSON.stringify(JSON.parse(response)), {
             headers: {'Content-Type': "application/json"}
         })
     }
-    return new Response(JSON.stringify(response), {
+    return new Response(response, {
         headers: {'Content-Type': "text/plain"}
     })
 }
