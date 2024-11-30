4adc0833656ccad3623361d88bac8532d5199a5e
Shall0e <shallowsspicymemes@gmail.com>
2024-11-29 23:38:42 -0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -8,7 +8,14 @@ function isJsonString(str) {
 }
 
 export async function onRequest(context) {
-    let response = await (await fetch("https://rers.shall0e.workers.dev/?"+((context.request.url).split("?"))[1])).text()
+    let search = ((context.request.url).split("?"))[1]
+
+    let response;
+    if (search == '') {
+        response = await (await fetch("https://rers.shall0e.workers.dev/")).text()
+    } else {
+        response = await (await fetch("https://rers.shall0e.workers.dev/?"+((context.request.url).split("?"))[1])).text()
+    }
 
     if (isJsonString(response)) {
         return new Response(JSON.stringify(JSON.parse(response)), {
