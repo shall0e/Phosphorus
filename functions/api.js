34d855de45a96e1ef2fd80f972475117e2a7cf02
Shall0e <shallowsspicymemes@gmail.com>
2024-11-29 23:27:30 -0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,22 @@
+function isJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
 export async function onRequest(context) {
-    let response = (await fetch("https://rers.shall0e.workers.dev/?"+((context.request.url).split("?"))[1])).json()
-    return new Response(JSON.stringify(await response), {
-        headers: {'Content-Type': "application/json"}
+    let response = await (await fetch("https://rers.shall0e.workers.dev/?"+((context.request.url).split("?"))[1])).json()
+
+    if (isJsonString(response)) {
+        return new Response(JSON.stringify(response), {
+            headers: {'Content-Type': "application/json"}
+        })
+    }
+    return new Response(response, {
+        headers: {'Content-Type': "text/plain"}
     })
 }
   //aaaa
