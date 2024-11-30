312b92a3404b0c4dd2a21cd2878b0cdd2b7b3bf3
Shall0e <shallowsspicymemes@gmail.com>
2024-11-30 00:42:20 -0500

    Update helloworld.js

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -9,7 +9,7 @@ export async function onRequest(context) {
     }
 
 
-    let response = await (await fetch("https://rers.shall0e.workers.dev/?"+(context.request.url).split("?")[1])).text()
+    let response = await (await fetch("https://rers.shall0e.workers.dev/"+(context.request.url).split("?")[1])).text()
     return new Response(response)
 
 }
