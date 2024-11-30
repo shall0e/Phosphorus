538752d596e3ae6172cd9207290ce80be067e914
Shall0e <shallowsspicymemes@gmail.com>
2024-11-30 00:51:50 -0500

    Update helloworld.js

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -15,12 +15,10 @@ export async function onRequest(context) {
         response = await (await fetch("https://rers.shall0e.workers.dev/")).text()
     }
 
-
-    return new Response(response)
     if (!isJsonString(response)) {
         return new Response(response, {headers: {"Content-Type": "text/plain"}})
     } else {
         return new Response(response, {headers: {"Content-Type": "application/json"}})
     }
 }
-  //aaaa
+  //aaaaa
