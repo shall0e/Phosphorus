a0534e225e48fdc182dd8abf43a3a3f31aa927be
Shall0e <shallowsspicymemes@gmail.com>
2024-11-29 23:19:29 -0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,5 @@
 export async function onRequest(context) {
-    let response = (await fetch("https://rers.shall0e.workers.dev/"+context.request.url)).text()
-    return new Response(((context.request.url).split("?"))[1])
-    // return new Response(JSON.stringify(await response))
+    let response = (await fetch("https://rers.shall0e.workers.dev/?"+((context.request.url).split("?"))[1])).text()
+    return new Response(JSON.stringify(await response))
 }
   //aaaa
