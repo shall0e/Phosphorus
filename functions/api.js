d3f6462bd802bb06f0f97633a2e81a7526835fdf
Shall0e <shallowsspicymemes@gmail.com>
2024-11-30 00:15:27 -0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,3 @@
-function isJsonString(str) {
-    try {
-        JSON.parse(str);
-    } catch (error) {
-        return false;
-    }
-    return true;
-}
-
 export async function onRequest(context) {
     response = await (await fetch("https://rers.shall0e.workers.dev/")).text()
     return new Response(response)
