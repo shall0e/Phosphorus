85e8000be50390cd4629ffee95eff9b7a6a7e24b
Shall0e <shallowsspicymemes@gmail.com>
2024-11-30 00:31:26 -0500

    uuyi

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-    let response = (await fetch("https://rers.shall0e.workers.dev/")).text()
+    let response = (await fetch("https://rers.shall0e.workers.dev/?"+(context.request.url).split("?")[1])).text()
     return new Response(JSON.stringify(await response))
 }
   //aaaa
