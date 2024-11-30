ee4c3f0c433d70fb670bebfba6b8a9913ff3cc7c
Shall0e <shallowsspicymemes@gmail.com>
2024-11-30 00:36:17 -0500

    Update helloworld.js

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -14,7 +14,7 @@ export async function onRequest(context) {
     if (!isJsonString(await response.text())) {
         return new Response(await response.text(),{headers: {"Content-Type": "text/plain"}})
     } else {
-        return new Response(JSON.stringify(await response.json()),{headers: {"Content-Type": "text/plain"}})
+        return new Response(JSON.stringify(await response.json()),{headers: {"Content-Type": "application/json"}})
     }
 
 
