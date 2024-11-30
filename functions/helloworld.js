b17b68ef0e7a4648aba6c5f5c7508cf41ff546d8
Shall0e <shallowsspicymemes@gmail.com>
2024-11-30 00:36:03 -0500

    Update helloworld.js

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,5 +1,22 @@
 export async function onRequest(context) {
-    let response = (await fetch("https://rers.shall0e.workers.dev/?"+(context.request.url).split("?")[1])).text()
-    return new Response(JSON.stringify(await response))
+    function isJsonString(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
+
+    let response = (await fetch("https://rers.shall0e.workers.dev/?"+(context.request.url).split("?")[1]))
+
+    if (!isJsonString(await response.text())) {
+        return new Response(await response.text(),{headers: {"Content-Type": "text/plain"}})
+    } else {
+        return new Response(JSON.stringify(await response.json()),{headers: {"Content-Type": "text/plain"}})
+    }
+
+
 }
   //aaaa
