4c6493967ad76646722c82454d00fe15083778d0
Shall0e <shallowsspicymemes@gmail.com>
2024-11-30 00:06:47 -0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -10,7 +10,7 @@ function isJsonString(str) {
 export async function onRequest(context) {
     let search = ((context.request.url).split("?"))[1]
     response = await (await fetch("https://rers.shall0e.workers.dev/?"+search)).text()
-
+    return new Response(response)
 
     if (isJsonString(response)) {
         return new Response(JSON.stringify(JSON.parse(response)), {
