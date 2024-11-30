7a61372b98ac591cc807f9353c3d4708ebdf67d1
Shall0e <shallowsspicymemes@gmail.com>
2024-11-29 23:35:13 -0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -8,9 +8,9 @@ function isJsonString(str) {
 }
 
 export async function onRequest(context) {
-    let response = await (await fetch("https://rers.shall0e.workers.dev/?"+((context.request.url).split("?"))[1])).json()
+    let response = await (await fetch("https://rers.shall0e.workers.dev/?"+((context.request.url).split("?"))[1])).text()
 
-    if (isJsonString(response) == true) {
+    if (isJsonString(response)) {
         return new Response(JSON.stringify(response), {
             headers: {'Content-Type': "application/json"}
         })
