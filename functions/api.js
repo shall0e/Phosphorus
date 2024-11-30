0af43646dea49d39ac02249a5edb9ccdaa58ed1d
Shall0e <shallowsspicymemes@gmail.com>
2024-11-29 23:34:15 -0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,7 @@
 function isJsonString(str) {
     try {
         JSON.parse(str);
-    } catch (e) {
+    } catch (error) {
         return false;
     }
     return true;
@@ -10,14 +10,13 @@ function isJsonString(str) {
 export async function onRequest(context) {
     let response = await (await fetch("https://rers.shall0e.workers.dev/?"+((context.request.url).split("?"))[1])).json()
 
-    if (!isJsonString(response)) {
-        return new Response(response, {
-            headers: {'Content-Type': "text/plain"}
-        })
-    } else {
+    if (isJsonString(response) == true) {
         return new Response(JSON.stringify(response), {
             headers: {'Content-Type': "application/json"}
         })
     }
+    return new Response(JSON.stringify(response), {
+        headers: {'Content-Type': "text/plain"}
+    })
 }
   //aaaa
