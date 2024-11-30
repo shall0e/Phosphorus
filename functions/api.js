02160befd7f6eea6eebe81bce7238ccdae963c3e
Shall0e <shallowsspicymemes@gmail.com>
2024-11-30 00:11:42 -0500

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -8,22 +8,7 @@ function isJsonString(str) {
 }
 
 export async function onRequest(context) {
-    if ((context.request.url).includes("?")) {
-        search = ("?"+((context.request.url).split("?"))[1]);
-    } else {
-        search = "";
-    }
     response = await (await fetch("https://rers.shall0e.workers.dev/"+search)).text()
     return new Response(response)
-
-    if (isJsonString(response)) {
-        return new Response(JSON.stringify(JSON.parse(response)), {
-            headers: {'Content-Type': "text/plain"}
-        })
-    } else {
-        return new Response(response, {
-            headers: {'Content-Type': "text/plain"}
-        })
-    }
 }
   //aaaa
