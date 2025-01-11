dad33d2174eb5901cadf6807448bc5836895d48a
Shall0e <shallowsspicymemes@gmail.com>
2025-01-11 14:11:39 -0500

    Update docs.js

--- functions/api/docs.js
+++ functions/api/docs.js
@@ -1,7 +1,20 @@
 export async function onRequest(context) {
-
-    let response = hi    
-    return new Response(response, {headers: {
+    let content;
+    fetch('https://example.com')
+        .then(response => {
+            if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text(); // Get the response as plain text
+        })
+        .then(html => {
+            content = html; // Log the HTML content
+        })
+        .catch(error => {
+            console.error('Error fetching the HTML page:', error);
+        });
+        
+    return new Response(content, {headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
