dced9fc9a54e44fab9ad15efe731a13a897285cf
Shall0e <shallowsspicymemes@gmail.com>
2025-01-11 14:10:09 -0500

    Update docs.js

--- functions/api/docs.js
+++ functions/api/docs.js
@@ -1,19 +1,6 @@
 export async function onRequest(context) {
 
-    fetch('https://shall0e.gitbook.io/ankor')
-        .then(response => {
-            if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.text(); // Get the response as plain text
-        })
-        .then(html => {
-            console.log(html); // Log the HTML content
-        })
-        .catch(error => {
-            console.error('Error fetching the HTML page:', error);
-        });
-
+    let response = hi    
     return new Response(response, {headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
